9d02c82f7073f06b64e5ed4f4b32026e90a57357
Edgar <edgarpinheiro3@gmail.com>
2023-06-12 13:07:47 -0300

    Listando todos os Produtos

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
+    private $product;
+
+    public function __construct(Product $product){
+        $this->product = $product;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +21,10 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return response()->json(['success' => 'Index']);
+
+        $products = $this->product->all();
+
+        return response()->json(['data' => $products]);
     }
 
     /**
