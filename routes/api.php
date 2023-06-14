7f53c6619d652133b3db607d4d77a04f4d622a8d
Edgar <edgarpinheiro3@gmail.com>
2023-06-14 19:17:33 -0300

    Configurar o JWT

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,42 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\ProductController;
+
+/*
+Route::prefix('v1')->controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+});
 
 Route::prefix('v1')->group(function () {
 
     Route::post('/products/search', [ProductController::class, 'search'])->name('products.search');
     
+    Route::resource('products', ProductController::class, 
+        ['except' =>[
+            'create',
+            'edit'
+        ]
+    ]);
+
+});
+*/
+
+Route::group([
+    'middleware' => 'api', 'auth',
+    'prefix' => 'v1'
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+    Route::post('logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::post('refresh', [AuthController::class, 'refresh'])->name('auth.refresh');
+
+    Route::post('/products/search', [ProductController::class, 'search'])->name('products.search');
+
     Route::resource('products', ProductController::class, 
         ['except' =>[
             'create',
